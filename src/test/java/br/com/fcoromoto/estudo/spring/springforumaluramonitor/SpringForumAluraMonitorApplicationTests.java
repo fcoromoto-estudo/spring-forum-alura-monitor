4e8df2c8b2c3a5cd96181d66e717aa0511aa6260
Francisco Coromoto <fcsouza1@stefanini.com>
2020-10-26 21:14:17 -0300

    first commit

---
+++ src/test/java/br/com/fcoromoto/estudo/spring/springforumaluramonitor/SpringForumAluraMonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.fcoromoto.estudo.spring.springforumaluramonitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringForumAluraMonitorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
